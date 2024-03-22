4479011e9d4398cc50942114aad339eff5cbdd69
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-03-22 13:52:33 +0900

    AuthMethodクラス作成、signUpUser関数でユーザー登録の処理を実装

---
+++ lib/resource/auth_methods.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:twitch_flutter/models/user.dart' as model;
+import 'package:twitch_flutter/providers/user_provider.dart';
+// import 'package:twitch_flutter/providers/user_provider.dart';
+import 'package:twitch_flutter/utils/utils.dart';
+import 'package:provider/provider.dart';
+
+class AuthMethods {
+  final _userRef = FirebaseFirestore.instance.collection('users');
+  final _auth = FirebaseAuth.instance;
+
+  Future<bool> signUpUser(BuildContext context, String email, String password,
+      String username) async {
+    bool res = false;
+    try {
+      UserCredential cred = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      if (cred.user != null) {
+        model.User user = model.User(
+          username: username.trim(),
+          email: email.trim(),
+          uid: cred.user!.uid,
+        );
+        await _userRef.doc(cred.user!.uid).set(user.toMap());
+        Provider.of<UserProvider>(context, listen: false).setUser(user);
+        res = true;
+      }
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!);
+      res = false;
+    }
+    return res;
+  }
+}
