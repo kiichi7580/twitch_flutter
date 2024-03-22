122759f0967c0e723afc8cc371e932d066531d11
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-03-22 13:50:10 +0900

    providerの初期設定追加、userProbider使用可能に設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:twitch_flutter/providers/user_provider.dart';
+import 'package:twitch_flutter/screens/home_screen.dart';
 import 'package:twitch_flutter/screens/login_screen.dart';
 import 'package:twitch_flutter/screens/onboarding_screen.dart';
 import 'package:twitch_flutter/screens/signup_screen.dart';
 import 'package:twitch_flutter/utils/colors.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -35,6 +50,7 @@ class MyApp extends StatelessWidget {
         OnBoardingScreen.routeName: (context) => const OnBoardingScreen(),
         LoginScreen.routeName: (context) => const LoginScreen(),
         SignupScreen.routeName: (context) => const SignupScreen(),
+        HomeScreen.routeName: (context) => const HomeScreen(),
       },
       home: const OnBoardingScreen(),
     );
