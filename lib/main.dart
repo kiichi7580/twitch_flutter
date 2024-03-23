0f2b0acfd34aae5aa98d509b52cd4d95db1b886e
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-03-23 23:52:15 +0900

    ユーザーのログイン状態を監視する処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:twitch_flutter/providers/user_provider.dart';
+import 'package:twitch_flutter/resource/auth_methods.dart';
 import 'package:twitch_flutter/screens/home_screen.dart';
 import 'package:twitch_flutter/screens/login_screen.dart';
 import 'package:twitch_flutter/screens/onboarding_screen.dart';
 import 'package:twitch_flutter/screens/signup_screen.dart';
 import 'package:twitch_flutter/utils/colors.dart';
+import 'package:twitch_flutter/models/user.dart' as model;
+import 'package:twitch_flutter/widgets/loading_indicator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -52,7 +56,32 @@ class MyApp extends StatelessWidget {
         SignupScreen.routeName: (context) => const SignupScreen(),
         HomeScreen.routeName: (context) => const HomeScreen(),
       },
-      home: const OnBoardingScreen(),
+      home: FutureBuilder(
+        future: AuthMethods()
+            .getCurrentUser(FirebaseAuth.instance.currentUser != null
+                ? FirebaseAuth.instance.currentUser!.uid
+                : null)
+            .then(
+          (value) {
+            if (value != null) {
+              Provider.of<UserProvider>(context, listen: false).setUser(
+                model.User.fromMap(value),
+              );
+            }
+            return value;
+          },
+        ),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingIndicator();
+          }
+
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+          return const OnBoardingScreen();
+        },
+      ),
     );
   }
 }
